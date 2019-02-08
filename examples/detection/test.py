36c93afe6d86afbb0d8188b6b7d38c91c58a3c05
swcho84 <swcho@nearthlab.com>
2019-02-08 16:45:33 +0900

    Set for Jetson

--- examples/detection/test.py
+++ examples/detection/test.py
@@ -44,9 +44,8 @@ trt_graph = trt.create_inference_graph(
     input_graph_def=frozen_graph,
     outputs=output_names,
     max_batch_size=1,
-    maximum_cached_engines=3,
     max_workspace_size_bytes=1 << 25,
-    precision_mode='FP32',
+    precision_mode='FP16',
     minimum_segment_size=50
 )
 
