6d1b247b58569a7313eac669e70314477950582e
Suho Park <hoo3302@gmail.com>
2019-02-11 18:43:01 +0900

    Set for trained Data
    Setting for out trained data
    and show multiple image result

--- examples/detection/test.py
+++ examples/detection/test.py
@@ -10,120 +10,134 @@ import matplotlib.patches as patches
 import tensorflow as tf
 import numpy as np
 import time
-from tf_trt_models.detection import download_detection_model, build_detection_graph, read_detection_model
+from tf_trt_models.detection import download_detection_model, build_detection_graph, read_trained_model, read_optimized_model
+from object_detection.utils import visualization_utils as vis_util
 
 print("**TEST Start**")
 
 # from tf_trt_models.detection import download_detection_model,build_detection_graph
 
 
+BOXES_NAME='detection_boxes'
+CLASSES_NAME='detection_classes'
+SCORES_NAME='detection_scores'
+NUM_DETECTIONS_NAME='num_detections'
 
-MODEL = 'faster_rcnn_inception_v2_coco'
+USED_TRAINED_DATA = True
+SAVED_RESULT = False
+MODEL = 'faster_rcnn_resnet101'
 DATA_DIR = './data/'
 DEST_DIR = './dest/'
-CONFIG_FILE = MODEL + '.config'   # ./data/ssd_inception_v2_coco.config 
-CHECKPOINT_FILE = 'model.ckpt'    # ./data/ssd_inception_v2_coco/model.ckpt
-IMAGE_PATH = './data/huskies.jpg'
-FROZEN_PATH = './dest/frozen/'
+CONFIG_FILE = 'pipeline.config'
+CHECKPOINT_PREFIX = 'model.ckpt-200000'
+IMAGE_DIR_PATH = os.path.join(DATA_DIR,'images')
+TRAINDED_DIR_PATH = os.path.join(DATA_DIR, 'trained_model')
+OPTIMIZED_PATH = os.path.join(DEST_DIR, 'frozen')
+
+category_index = {0:{'name':'background'}, 1:{'name':'blade'}}
 # Download the pretrained model
 # Download the model configuration file and checkpoint containing pretrained weights by using the following command.
 # For improved performance, increase the non-max suppression score threshold in the downloaded config file from 1e-8 to something greater, like 0.1.
-'''
-config_path, checkpoint_path = download_detection_model(MODEL, 'data')
+if __name__ == '__main__':
+    if os.path.exists(os.path.join(OPTIMIZED_PATH, MODEL+'_trt.pb')):
+        # already optimzed the model
+        trt_graph = read_optimized_model(MODEL,OPTIMIZED_PATH)
+    else:
+        if os.path.exists(os.path.join(TRAINDED_DIR_PATH, 'frozen_inference_graph_'+MODEL+'.pb')):
+            # trained Model not optimized
+            TRAINED_MODEL = 'frozen_inference_graph_'+MODEL+'.pb'
+            frozen_graph = read_trained_model(TRAINED_MODEL, TRAINDED_DIR_PATH)
+        else:
+            if USED_TRAINED_DATA:
+                print("Using trained Data")
+                config_path = os.path.join(DATA_DIR,'trained_data',MODEL,CONFIG_FILE)
+                checkpoint_path = os.path.join(DATA_DIR,'trained_data',MODEL,CHECKPOINT_PREFIX)
+            else :
+                config_path, checkpoint_path = download_detection_model(MODEL, 'data')
+            frozen_graph = build_detection_graph(
+                config=config_path,
+                checkpoint=checkpoint_path,
+                batch_size=1,
+                force_nms_cpu=True
+            )
+        output_names = [BOXES_NAME, CLASSES_NAME, SCORES_NAME, NUM_DETECTIONS_NAME]
+        trt_graph = trt.create_inference_graph(
+            input_graph_def=frozen_graph,
+            outputs=output_names,
+            max_batch_size=1,
+            maximum_cached_engines=3,
+            max_workspace_size_bytes=1 << 20,
+            precision_mode='FP32',
+            minimum_segment_size=20
+        )
+        with open(os.path.join(DEST_DIR,'frozen',MODEL+'_trt.pb'), 'wb') as f:
+            f.write(trt_graph.SerializeToString())
 
-frozen_graph, input_names, output_names = build_detection_graph(
-    config=config_path,
-    checkpoint=checkpoint_path,
-    batch_size=1
-)
-'''
-if os.path.exists(os.path.join(FROZEN_PATH, MODEL+'_trt.pb')):
-    trt_graph = read_detection_model(MODEL,FROZEN_PATH)
-else:
-    config_path, checkpoint_path = download_detection_model(MODEL, 'data')
+    print("Get the trt graph done")
 
-    frozen_graph, input_names, output_names = build_detection_graph(
-        config=config_path,
-        checkpoint=checkpoint_path,
-        batch_size=1
-    )
-    trt_graph = trt.create_inference_graph(
-        input_graph_def=frozen_graph,
-        outputs=output_names,
-        max_batch_size=1,
-        max_workspace_size_bytes=1 << 25,
-        precision_mode='FP16',
-        minimum_segment_size=50
-    )
-    with open(os.path.join(DEST_DIR,'frozen',MODEL+'_trt.pb'), 'wb') as f:
-        f.write(trt_graph.SerializeToString())
+    # Create session and load graph
+    def load_image_into_numpy_array(image):
+        (im_width, im_height) = image.size
+        # print(image.size)
+        return np.array(image.getdata()).reshape(
+            (im_height, im_width, 3)).astype(np.uint8)
 
-    
-print("Get the trt graph done")
+    # For the sake of simplicity we will use only 2 images:
+    # image1.jpg
+    # image2.jpg
+    # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+    TEST_IMAGE_PATHS = sorted(os.listdir(IMAGE_DIR_PATH))
+    TEST_IMAGE_PATHS = [os.path.join(IMAGE_DIR_PATH, filename) for filename in TEST_IMAGE_PATHS]
+    # Size, in inches, of the output images.
 
-# Create session and load graph
 
-tf_config = tf.ConfigProto()
-tf_config.gpu_options.allow_growth = True
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
 
-tf_sess = tf.Session(config=tf_config)
+    with tf.Session(config=tf_config) as tf_sess:
+        tf.import_graph_def(trt_graph, name='')
 
-tf.import_graph_def(trt_graph, name='')
+        tf_input = tf_sess.graph.get_tensor_by_name( 'image_tensor:0')
+        tf_scores = tf_sess.graph.get_tensor_by_name('detection_scores:0')
+        tf_boxes = tf_sess.graph.get_tensor_by_name('detection_boxes:0')
+        tf_classes = tf_sess.graph.get_tensor_by_name('detection_classes:0')
+        tf_num_detections = tf_sess.graph.get_tensor_by_name('num_detections:0')
+        elapsed_times = []
+        print("Total image number %d" % len(TEST_IMAGE_PATHS))
+        for image_path in TEST_IMAGE_PATHS[:100]:
+            image = Image.open(image_path)
+            # Load and Preprocess Image
+            # the array based representation of the image will be used later in order to prepare the
+            # result image with boxes and labels on it.
+            image_np = load_image_into_numpy_array(image)
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            # Actual detection.
+            t0 = time.time()
+            (boxes, scores, classes, num) = tf_sess.run(
+                [tf_boxes, tf_scores, tf_classes, tf_num_detections],
+                feed_dict={tf_input: image_np_expanded})
+            t1 = time.time()
+            print("elapsed time (ms) : ", float(t1-t0)*1000)
+            elapsed_times.append(float(t1-t0)*1000)
+            # print("classes",np.squeeze(classes).astype(np.int32))
+            # print("num detection",num)
+            if SAVED_RESULT:
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                        image_np,
+                        np.squeeze(boxes),
+                        np.squeeze(classes).astype(np.int32),
+                        np.squeeze(scores),
+                        category_index,
+                        use_normalized_coordinates=True,
+                        line_thickness=8)
+                plt.figure()
+                plt.imshow(image_np)
+                plt.savefig('dest/test_result/%s/%s' % (MODEL,os.path.basename(image_path)))
+                print("saved %s" % os.path.basename(image_path))
+                plt.close()
+            del image
+        TRASH = max(elapsed_times) # The First data makes dirty
+        print("avg. elapsed time: ", (sum(elapsed_times) - TRASH) / (len(elapsed_times) - 1))
 
-tf_input = tf_sess.graph.get_tensor_by_name( 'image_tensor:0')
-tf_scores = tf_sess.graph.get_tensor_by_name('detection_scores:0')
-tf_boxes = tf_sess.graph.get_tensor_by_name('detection_boxes:0')
-tf_classes = tf_sess.graph.get_tensor_by_name('detection_classes:0')
-tf_num_detections = tf_sess.graph.get_tensor_by_name('num_detections:0')
-
-# Load and Preprocess Image
-
-image = Image.open(IMAGE_PATH)
-
-plt.imshow(image)
-plt.savefig(os.path.join(DEST_DIR, 'first.jpg'))
-image_resized = np.array(image.resize((300, 300)))
-image = np.array(image)
-# Run network on Image
-print("Try to inference")
-scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
-    tf_input: image_resized[None, ...]
-})
-print("Inference Done")
-
-boxes = boxes[0] # index by 0 to remove batch dimension
-scores = scores[0]
-classes = classes[0]
-num_detections = num_detections.astype(int)[0]
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-
-ax.imshow(image)
-
-# plot boxes exceeding score threshold
-for i in range(num_detections):
-    # scale box to image coordinates
-    box = boxes[i] * np.array([image.shape[0], image.shape[1], image.shape[0], image.shape[1]])
-
-    # display rectangle
-    patch = patches.Rectangle((box[1], box[0]), box[3] - box[1], box[2] - box[0], color='g', alpha=0.3)
-    ax.add_patch(patch)
-
-    # display class index and score
-    plt.text(x=box[1] + 10, y=box[2] - 10, s='%d (%0.2f) ' % (classes[i], scores[i]), color='w')
-
-# plt.show()
-plt.savefig(os.path.join(DEST_DIR, MODEL+'_result.jpg'))
-
-num_samples = 50
-print("Checking time .....")
-t0 = time.time()
-for i in range(num_samples):
-    scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
-        tf_input: image_resized[None, ...]
-    })
-t1 = time.time()
-print('Average runtime: %f ms' % (float(t1 - t0) * 1000 / num_samples))
-
-tf_sess.close()
+        tf_sess.close()
--- tf_trt_models/detection.py
+++ tf_trt_models/detection.py
@@ -13,15 +13,12 @@ import tensorflow as tf
 from .graph_utils import force_nms_cpu as f_force_nms_cpu
 from .graph_utils import replace_relu6 as f_replace_relu6
 from .graph_utils import remove_assert as f_remove_assert
+from .graph_utils import expand_dim as f_expand_dim
 
 DetectionModel = namedtuple('DetectionModel', ['name', 'url', 'extract_dir'])
 
 INPUT_NAME='image_tensor'
-BOXES_NAME='detection_boxes'
-CLASSES_NAME='detection_classes'
-SCORES_NAME='detection_scores'
 MASKS_NAME='detection_masks'
-NUM_DETECTIONS_NAME='num_detections'
 FROZEN_GRAPH_NAME='frozen_inference_graph.pb'
 PIPELINE_CONFIG_NAME='pipeline.config'
 CHECKPOINT_PREFIX='model.ckpt'
@@ -94,7 +91,7 @@ def get_output_names(model):
 
 def download_detection_model(model, output_dir='.'):
     """Downloads a pre-trained object detection model"""
-    print("Real File******************************")
+    print("Readownload_detection_model")
     global MODELS
 
     model_name = model
@@ -115,7 +112,6 @@ def download_detection_model(model, output_dir='.'):
 
     return config_path, checkpoint_path
 
-
 def build_detection_graph(config, checkpoint,
         batch_size=1,
         score_threshold=None,
@@ -125,46 +121,51 @@ def build_detection_graph(config, checkpoint,
         input_shape=None,
         output_dir='.generated_model'):
     """Builds a frozen graph for a pre-trained object detection model"""
-    
+
     config_path = config
     checkpoint_path = checkpoint
 
     # parse config from file
     config = pipeline_pb2.TrainEvalPipelineConfig()
-    with open(config_path, 'r') as f:
-        google.protobuf.text_format.Merge(f.read(), config, allow_unknown_extension=True)
 
-    # override some config parameters
-    if config.model.HasField('ssd'):
-        config.model.ssd.feature_extractor.override_base_feature_extractor_hyperparams = True
-        if score_threshold is not None:
-            config.model.ssd.post_processing.batch_non_max_suppression.score_threshold = score_threshold    
-        if input_shape is not None:
-            config.model.ssd.image_resizer.fixed_shape_resizer.height = input_shape[0]
-            config.model.ssd.image_resizer.fixed_shape_resizer.width = input_shape[1]
-    elif config.model.HasField('faster_rcnn'):
-        if score_threshold is not None:
-            config.model.faster_rcnn.second_stage_post_processing.batch_non_max_suppresion.score_threshold = score_threshold
-        if input_shape is not None:
-            config.model.faster_rcnn.image_resizer.fixed_shape_resizer.height = input_shape[0]
-            config.model.faster_rcnn.image_resizer.fixed_shape_resizer.width = input_shape[1]
+    if not os.path.exists(os.path.join(output_dir, FROZEN_GRAPH_NAME)):
+        print("Makes New Graph")
+        with open(config_path, 'r') as f:
+            google.protobuf.text_format.Merge(f.read(), config, allow_unknown_extension=True)
 
-    if os.path.isdir(output_dir):
-        subprocess.call(['rm', '-rf', output_dir])
+        # override some config parameters
+        if config.model.HasField('ssd'):
+            config.model.ssd.feature_extractor.override_base_feature_extractor_hyperparams = True
+            if score_threshold is not None:
+                config.model.ssd.post_processing.batch_non_max_suppression.score_threshold = score_threshold
+            if input_shape is not None:
+                config.model.ssd.image_resizer.fixed_shape_resizer.height = input_shape[0]
+                config.model.ssd.image_resizer.fixed_shape_resizer.width = input_shape[1]
+        elif config.model.HasField('faster_rcnn'):
+            if score_threshold is not None:
+                config.model.faster_rcnn.second_stage_post_processing.batch_non_max_suppresion.score_threshold = score_threshold
+            if input_shape is not None:
+                config.model.faster_rcnn.image_resizer.fixed_shape_resizer.height = input_shape[0]
+                config.model.faster_rcnn.image_resizer.fixed_shape_resizer.width = input_shape[1]
 
-    tf_config = tf.ConfigProto()
-    tf_config.gpu_options.allow_growth = True
+        if os.path.isdir(output_dir):
+            subprocess.call(['rm', '-rf', output_dir])
 
-    # export inference graph to file (initial)
-    with tf.Session(config=tf_config) as tf_sess:
-        with tf.Graph().as_default() as tf_graph:
-            exporter.export_inference_graph(
-                'image_tensor', 
-                config, 
-                checkpoint_path, 
-                output_dir, 
-                input_shape=[batch_size, None, None, 3]
-            )
+        tf_config = tf.ConfigProto()
+        tf_config.gpu_options.allow_growth = True
+
+        # export inference graph to file (initial)
+        with tf.Session(config=tf_config) as tf_sess:
+            with tf.Graph().as_default() as tf_graph:
+                exporter.export_inference_graph(
+                    'image_tensor', 
+                    config, 
+                    checkpoint_path, 
+                    output_dir, 
+                    input_shape=[batch_size, None, None, 3]
+                )
+    else:
+        print("Using Exist Exported Graph")
 
     # read frozen graph from file
     frozen_graph = tf.GraphDef()
@@ -178,31 +179,38 @@ def build_detection_graph(config, checkpoint,
         frozen_graph = f_replace_relu6(frozen_graph)
     if remove_assert:
         frozen_graph = f_remove_assert(frozen_graph)
-
-    # get input names
-    # TODO: handle mask_rcnn 
-    input_names = [INPUT_NAME]
-    output_names = [BOXES_NAME, CLASSES_NAME, SCORES_NAME, NUM_DETECTIONS_NAME]
-
     # remove temporary directory
     subprocess.call(['rm', '-rf', output_dir])
 
-    return frozen_graph, input_names, output_names
+    return frozen_graph
 
-def read_detection_model(model, frozen_dir='./dest/frozen/',
-	force_nms_cpu=True,
-	replace_relu6=True,
-	remove_assert=True
+# for Non optimized traied model
+def read_trained_model(TRAINED_MODEL, TRAINDED_DIR_PATH='./data/trained_model/',
+    force_nms_cpu=True,
+    replace_relu6=True,
+    remove_assert=True
 ):
-    print("Read %s Model" % (model))
-    global MODELS
+    print("Read trained %s Model" % (TRAINED_MODEL))
+    # read frozen graph from file
+    frozen_graph = tf.GraphDef()
+    with open(os.path.join(TRAINDED_DIR_PATH, TRAINED_MODEL), 'rb') as f:
+        frozen_graph.ParseFromString(f.read())
 
-    model_name = model
+    # apply graph modifications
+    if force_nms_cpu:
+        frozen_graph = f_force_nms_cpu(frozen_graph)
+    if replace_relu6:
+        frozen_graph = f_replace_relu6(frozen_graph)
+    if remove_assert:
+        frozen_graph = f_remove_assert(frozen_graph)
 
-    model = MODELS[model_name]
+    return frozen_graph
+
+def read_optimized_model(model, frozen_dir='./dest/frozen/'):
+    print("Read optimized %s Model" % (model))
 
     trt_graph = tf.GraphDef()
-    with open(os.path.join(frozen_dir, model_name+'_trt.pb'), 'rb') as f:
+    with open(os.path.join(frozen_dir, model+'_trt.pb'), 'rb') as f:
         trt_graph.ParseFromString(f.read())
 
     return trt_graph
